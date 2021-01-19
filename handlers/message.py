1c96b372cc56349f02a8f9eed1ba5cc92b2f6642
DahaWong <dahawong@gmail.com>
2021-01-19 21:22:00 +0800

    Basic copy message.

--- handlers/message.py
+++ handlers/message.py
@@ -4,4 +4,4 @@ import callbacks.message as callback
 links_handler = MessageHandler(Filters.entity("url") & Filters.regex(r'#yeyu'), callback.send_to_channel)
 tv_handler = MessageHandler(Filters.regex(r'#上电视'), callback.send_to_channel)
 
-handlers=[links_handler]
+handlers=[links_handler, tv_handler]
