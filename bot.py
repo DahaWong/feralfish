1b91398ff837c661e99d91c1cdd457e37ee7df5a
DahaWong <dahawong@gmail.com>
2021-02-06 00:44:07 +0800

    Accept edited message update.

--- bot.py
+++ bot.py
@@ -10,7 +10,9 @@ from functools import wraps
 config = configparser.ConfigParser()
 config.read('config.ini')
 bot_token = config['BOT']['TOKEN']
+bot_token_test = config['BOT']['TOKEN_TEST']
 proxies = config['PROXIES']
+proxy = config['BOT']['PROXY']
 defaults = Defaults(
     parse_mode="MARKDOWN",
     disable_notification=True
@@ -18,16 +20,25 @@ defaults = Defaults(
 dev_user_id = int(config['DEV']['ID'])  # daha
 channel_owner = int(config['CHANNEL']['OWNER'])  # bob
 channel_id = config['CHANNEL']['ID']
+# update_info = {
+#     'token': bot_token,
+#     'use_context': True,
+#     'defaults': defaults
+# }
 update_info = {
-    'token': bot_token,
+    'token': bot_token_test,
     'use_context': True,
-    'defaults': defaults
+    'defaults': defaults,
+    'request_kwargs': {
+        'proxy_url': proxy
+    }
 }
 
 
 class manifest:
     name = "野鱼"
     channel_name = "野鱼日报"
+    group_name = "鮀浦镇"
     bot_id = "FeralFishBot"
     author = "Daha"
     author_id = 'dahawong'
@@ -46,7 +57,9 @@ dispatcher = updater.dispatcher
 def club(func):
     @wraps(func)
     def wrapped(update, context, *args, **kwargs):
-        message = update.message
+        message = update.message or update.edited_message
+        if not message:
+            return
         admins = message.chat.get_administrators()
         if admins and (channel_owner not in map(lambda x: x.user.id, admins)):
             message.reply_text('当前群聊还不是一个业余公司！')
@@ -69,7 +82,7 @@ def dev(func):
 
 def start(update, context):
     update.message.reply_text(
-        f"你好，我是{manifest.name}！[{manifest.channel_name}](https://t.me/{channel_id})的管理员。")
+        f"你好，我是{manifest.name}！[{manifest.group_name}](https://t.me/{channel_id})的管家。")
 
 
 def about(update, context):
@@ -102,7 +115,12 @@ def share(update, context, share_type='分享发现'):
     message = update.message
     replied_message = message.reply_to_message
     if not replied_message:
-        message.reply_text('推荐的内容不能为空 :)')
+        message.reply_text(
+            (
+                '嗨，想要使用指令，需要先*回复*一条想要分享的群组消息，并在回复的对话框中填入命令；被回复的消息会同步到野鱼日报。\n\n'
+                '有疑问可以找 @bob_fu 或 @dahawong :)'
+            )
+        )
         return
     new_message = replied_message.copy(chat_id=f'@{channel_id}')
     bot = context.bot
@@ -139,36 +157,36 @@ def help(update, context):
 
 @club
 def send_to_channel(update, context):
-    message = update.message
-    if not (message and message.from_user.id == 777000):
+    message = update.message or update.edited_message
+    if (not message) or message.from_user.id == 777000:
         return
     message.forward(chat_id=f'@{channel_id}')
 
 
-# Handle Error
-def handle_error(update, context):
-    if not update:
-        return
-    if update.effective_message:
-        text = f"刚刚的操作触发了一个错误，报告已抄送给[开发者](tg://user?id={dev_user_id})。"
-        update.effective_message.reply_text(text)
-    payload = ""
-    if update.effective_user:
-        payload += f"有[用户](tg://user?id={update.effective_user.id})"
-    if update.effective_chat.title:
-        payload += f"在{update.effective_chat.title}"
-        if update.effective_chat.username:
-            payload += f'(@{update.effective_chat.username})'
-    if update.poll:
-        payload += f'在发起投票 {update.poll.id} 时'
-    trace = "".join(traceback.format_tb(sys.exc_info()[2]))
-    text = f"{payload}触发了一个错误：`{context.error}`。\n\n"
-    text += f"错误路径如下:\n`{trace}`" if trace else ''
-    context.bot.send_message(dev_user_id, text)
+# # Handle Error
+# def handle_error(update, context):
+#     if not update:
+#         return
+#     if update.effective_message:
+#         text = f"刚刚的操作触发了一个错误，报告已抄送给[开发者](tg://user?id={dev_user_id})。"
+#         update.effective_message.reply_text(text)
+#     payload = ""
+#     if update.effective_user:
+#         payload += f"有[用户](tg://user?id={update.effective_user.id})"
+#     if update.effective_chat.title:
+#         payload += f"在{update.effective_chat.title}"
+#         if update.effective_chat.username:
+#             payload += f'(@{update.effective_chat.username})'
+#     if update.poll:
+#         payload += f'在发起投票 {update.poll.id} 时'
+#     trace = "".join(traceback.format_tb(sys.exc_info()[2]))
+#     text = f"{payload}触发了一个错误：`{context.error}`。\n\n"
+#     text += f"错误路径如下:\n`{trace}`" if trace else ''
+#     context.bot.send_message(dev_user_id, text)
 
 
 # Handlers
-dispatcher.add_error_handler(handle_error, run_async=True)
+# dispatcher.add_error_handler(handle_error, run_async=True)
 
 command_handlers = [
     CommandHandler('start', start),
@@ -178,8 +196,10 @@ command_handlers = [
     CommandHandler('yeyu', yeyu),
     CommandHandler('proxy', show_proxy)
 ]
-message_handlers = [MessageHandler(
-    Filters.regex(r'#(?:上电视|野鱼屏幕|分享发现)'), send_to_channel)]
+message_handlers = [
+    MessageHandler(Filters.chat_type.groups & Filters.update.edited_message & Filters.regex(r'#(?:上电视|野鱼屏幕|分享发现)'), send_to_channel),
+    MessageHandler(Filters.chat_type.groups & Filters.regex(r'#(?:上电视|野鱼屏幕|分享发现)'), send_to_channel)
+]
 handlers = command_handlers.extend(message_handlers)
 
 for handler in command_handlers:
