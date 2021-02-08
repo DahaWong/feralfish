c51b10ae43306b4d1620120177eaba2c6872e97a
DahaWong <dahawong@gmail.com>
2021-02-08 11:14:22 +0800

    Add more emojis.

--- feralfish/callback/poll.py
+++ feralfish/callback/poll.py
@@ -1,21 +1,24 @@
 from config import group_id
 import random
 moods = [10, 7.5, 5, 2.5, 0]
-weather = ['🌪', random.choice('⛈️','❄️','🌨'), '🌧', '☔️', '☂️', '🌦', '☁️',  '⛅️', '🌤', random.choice('🏝', '🏞️', '🏖', '🌅', '📈', '☀️'), '🏝']
+
 
 def handle_poll_answer(update, context):
+    weather = ['🌪', random.choice('⛈️', '❄️', '🌨'), '🌧', '☔️', '☂️', '🌦',
+               '☁️',  '⛅️', '🌤', random.choice('🏝', '🏞️', '🏖', '🌅', '📈', '☀️'), '🏝']
     if context.bot_data['poll_id'] != update.poll_answer.poll_id:
         return
 
     context.bot_data['count'] += 1
     mood = moods[update.poll_answer.option_ids[0]]
     context.bot_data['score'] += mood
-    weather_symbol = weather[round(context.bot_data['score']/context.bot_data['count'])]
+    weather_symbol = weather[round(
+        context.bot_data['score']/context.bot_data['count'])]
     # print(context.bot_data['score'])
     # print(context.bot_data['count'])
     # print(weather_symbol)
     context.bot.set_chat_title(group_id, f"{weather_symbol} 鮀浦镇｜灵感买家俱乐部")
-    
+
     # context.bot.set_chat_description(update.effective_chat.id, (
     #     f"太阳能维修，夜总会发光。\n\n"
     #     f"鮀浦镇今日天气：{weather_symbol}"
